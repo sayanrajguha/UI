44efc53487fb79b1104fc2953e49b1e4727cea2a
Sayanraj Guha <sayanrajguha@gmail.com>
2016-10-28 12:40:58 +0000

    added modals on index html

--- rms.peerctors.com/javascript/script.js
+++ rms.peerctors.com/javascript/script.js
@@ -20,5 +20,36 @@ $('#btnTool').on('click',function(e) {
 });
 $('.stackFilter').on('click', function() {
    $('.filterRow').toggleClass('hidden');
+   if($('.filterRow').hasClass('hidden')) {
+   	$('.linkCrudFilter').css('color','orange');
+   } else {
+   	$('.linkCrudFilter').css('color','#e6e6e6');
+   }
+});
+$('.stackRemove').on('click', function() {
+	$('.deleteColumn').toggle();
+	$('.slNo').toggle();
+	if($('.deleteColumn').is(':visible')) {
+   	$('.linkCrudRemove').css('color','#e6e6e6');
+   } else {
+   	$('.linkCrudRemove').css('color','red');
+   }
+});
+
+var dictionaryData = [
+      "Word 1",
+      "Word 2",
+      "Word 3",
+      "Word 4",
+      "Word 5"
+    ];
+$("#dictionarySearch").autocomplete({
+      source: dictionaryData
+});
+
+$('.datepicker').datepicker({
+	changeMonth: true,
+   changeYear: true,
+   dateFormat : 'dd-mm-yy'
 });
 });
