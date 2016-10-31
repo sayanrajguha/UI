c3779b263d0573680aa73d89beecf2ff1be0aa59
Sayanraj Guha <sayanrajguha@gmail.com>
2016-10-31 19:59:24 +0000

    ui mostly completed. completed user section, added popover on profile

--- rms.peerctors.com/javascript/script.js
+++ rms.peerctors.com/javascript/script.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
 
 
+$('#btnShowPass').on('click',function() {
+	$(this).toggleClass('pressedBtn');
+	if($('#password').attr('type') === 'password') {
+		$('#password').attr('type','text');
+	} else {
+		$('#password').attr('type','password');
+	}
+});
+
+
 $("#upcAccordian").on('click', function() {
 	$("#panel-element-58940").collapse('toggle');
 });
 
 $("#oodAccordian").on('click', function() {
 	$("#panel-element-137647").collapse('toggle');
+});
+$("#dictionaryAccordian").on('click', function() {
+	$("#panel-element-999").collapse('toggle');
 });
 $('#btnSite').on('click',function(e) {
 	$('#btnSite').toggleClass('active');
@@ -29,12 +42,18 @@ $('.stackFilter').on('click', function() {
 $('.stackRemove').on('click', function() {
 	$('.deleteColumn').toggle();
 	$('.slNo').toggle();
-	if($('.deleteColumn').is(':visible')) {
-   	$('.linkCrudRemove').css('color','#e6e6e6');
-   } else {
-   	$('.linkCrudRemove').css('color','red');
-   }
+	$('.crud').toggle();
+	$('.detailsCell').toggle();
+	$('.saveCancel').toggle();
 });
+$('.stackRemoveCancel').on('click', function() {
+	$('.deleteColumn').toggle();
+	$('.slNo').toggle();
+	$('.crud').toggle();
+	$('.detailsCell').toggle();
+	$('.saveCancel').toggle();
+});
+
 
 var dictionaryData = [
       "Word 1",
@@ -52,4 +71,12 @@ $('.datepicker').datepicker({
    changeYear: true,
    dateFormat : 'dd-mm-yy'
 });
+
+
+
+// showing popovers
+$('.dropdown-toggle').popover({
+   'placement':'left',
+   'content':'2'
+}).popover('show');
 });
